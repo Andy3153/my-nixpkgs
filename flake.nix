1bfbcd34daa9dd8e3ac02ed92431a5f6e3dcf236
Andy3153 <andy3153@protonmail.com>
2024-06-24 16:03:58 +0300

    init

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "Andy3153's collection of packages for Nix";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs = { nixpkgs, ... }:
+  {
+    packages.x86_64-linux =
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      hello = pkgs.callPackage ./pkgs/hello.nix { };
+    };
+  };
+}
