f954e736d02e5452be1a4fe44c59d6bc4ea7565d
Andy3153 <andy3153@protonmail.com>
2024-09-15 22:35:27 +0300

    flake: allow all derivations on all architectures
    though, this'll allow derivations to try and fail to compile on other architectures if they're not supported

--- flake.nix
+++ flake.nix
@@ -1,16 +1,16 @@
 {
   description = "Andy3153's collection of packages for Nix";
-
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
   outputs = { nixpkgs, ... }:
+  let
+    # {{{ Variables
+  supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+  forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f { pkgs = import nixpkgs { inherit system; }; });
+    # }}}
+  in
   {
-    packages.x86_64-linux =
-    let
-      # {{{ Variables
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      # }}}
-    in
+    packages = forEachSupportedSystem ({ pkgs }:
     {
       batnotifsd               = pkgs.callPackage ./pkgs/batnotifsd.nix               { };
       libratbag-git            = pkgs.callPackage ./pkgs/libratbag-git.nix            { };
@@ -18,6 +18,6 @@
       piper-git                = pkgs.callPackage ./pkgs/piper-git.nix                { };
       plymouth-spinnerv2-theme = pkgs.callPackage ./pkgs/plymouth-spinnerv2-theme.nix { };
       weather4bar              = pkgs.callPackage ./pkgs/weather4bar.nix              { };
-    };
+    });
   };
 }
